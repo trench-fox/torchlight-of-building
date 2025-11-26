9b161a5a3d7a363ee3ae3614081b16716e1574a7
a <aclghrand@gmail.com>
2025-11-25 16:51:35 -0800

    foo

--- src/tli/gear_affix_data/wand_base_affix.ts
+++ src/tli/gear_affix_data/wand_base_affix.ts
@@ -2,272 +2,272 @@ import { BaseGearAffix } from "./types";
 
 export const WAND_BASE_AFFIX_AFFIXES = [
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "0",
-    "template": "Max Agility Blessing Stacks +1",
-    "valueRanges": [],
-    "rawAffix": "Max Agility Blessing Stacks `+1`"
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "0",
+    template: "Max Agility Blessing Stacks +1",
+    valueRanges: [],
+    rawAffix: "Max Agility Blessing Stacks `+1`",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "0",
-    "template": "Max Focus Blessing Stacks +1",
-    "valueRanges": [],
-    "rawAffix": "Max Focus Blessing Stacks `+1`"
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "0",
+    template: "Max Focus Blessing Stacks +1",
+    valueRanges: [],
+    rawAffix: "Max Focus Blessing Stacks `+1`",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "0",
-    "template": "Max Tenacity Blessing Stacks +1",
-    "valueRanges": [],
-    "rawAffix": "Max Tenacity Blessing Stacks `+1`"
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "0",
+    template: "Max Tenacity Blessing Stacks +1",
+    valueRanges: [],
+    rawAffix: "Max Tenacity Blessing Stacks `+1`",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "1",
-    "template": "+{0}% Armor DMG Mitigation Penetration",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "1",
+    template: "+{0}% Armor DMG Mitigation Penetration",
+    valueRanges: [
       {
-        "min": 5,
-        "max": 7
-      }
+        min: 5,
+        max: 7,
+      },
     ],
-    "rawAffix": "`+(5-7)`% Armor DMG Mitigation Penetration"
+    rawAffix: "`+(5-7)`% Armor DMG Mitigation Penetration",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "1",
-    "template": "+{0}% Elemental and Erosion Resistance Penetration",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "1",
+    template: "+{0}% Elemental and Erosion Resistance Penetration",
+    valueRanges: [
       {
-        "min": 6,
-        "max": 8
-      }
+        min: 6,
+        max: 8,
+      },
     ],
-    "rawAffix": "`+(6-8)`% Elemental and Erosion Resistance Penetration"
+    rawAffix: "`+(6-8)`% Elemental and Erosion Resistance Penetration",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "1",
-    "template": "+1Parabolic Projectile Split Quantity",
-    "valueRanges": [],
-    "rawAffix": "`+1`Parabolic Projectile Split Quantity"
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "1",
+    template: "+1Parabolic Projectile Split Quantity",
+    valueRanges: [],
+    rawAffix: "`+1`Parabolic Projectile Split Quantity",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "1",
-    "template": "Projectile Quantity +1",
-    "valueRanges": [],
-    "rawAffix": "Projectile Quantity `+1`"
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "1",
+    template: "Projectile Quantity +1",
+    valueRanges: [],
+    rawAffix: "Projectile Quantity `+1`",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "+{0}% Elemental Damage",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "+{0}% Elemental Damage",
+    valueRanges: [
       {
-        "min": 20,
-        "max": 24
-      }
+        min: 20,
+        max: 24,
+      },
     ],
-    "rawAffix": "`+(20-24)`% Elemental Damage"
+    rawAffix: "`+(20-24)`% Elemental Damage",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "+{0}% Erosion Damage",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "+{0}% Erosion Damage",
+    valueRanges: [
       {
-        "min": 20,
-        "max": 24
-      }
+        min: 20,
+        max: 24,
+      },
     ],
-    "rawAffix": "`+(20-24)`% Erosion Damage"
+    rawAffix: "`+(20-24)`% Erosion Damage",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "+{0}% Physical Damage",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "+{0}% Physical Damage",
+    valueRanges: [
       {
-        "min": 20,
-        "max": 24
-      }
+        min: 20,
+        max: 24,
+      },
     ],
-    "rawAffix": "`+(20-24)`% Physical Damage"
+    rawAffix: "`+(20-24)`% Physical Damage",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "+{0}% Spell Damage",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "+{0}% Spell Damage",
+    valueRanges: [
       {
-        "min": 20,
-        "max": 24
-      }
+        min: 20,
+        max: 24,
+      },
     ],
-    "rawAffix": "`+(20-24)`% Spell Damage"
+    rawAffix: "`+(20-24)`% Spell Damage",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "+{0}% Cast Speed",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "+{0}% Cast Speed",
+    valueRanges: [
       {
-        "min": 6,
-        "max": 8
-      }
+        min: 6,
+        max: 8,
+      },
     ],
-    "rawAffix": "`+(6-8)`% Cast Speed"
+    rawAffix: "`+(6-8)`% Cast Speed",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "Adds {0}- {1}Lightning Damage to Spells",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "Adds {0}- {1}Lightning Damage to Spells",
+    valueRanges: [
       {
-        "min": 1,
-        "max": 2
+        min: 1,
+        max: 2,
       },
       {
-        "min": 28,
-        "max": 32
-      }
+        min: 28,
+        max: 32,
+      },
     ],
-    "rawAffix": "Adds `(1-2)`- `(28-32)`Lightning Damage to Spells"
+    rawAffix: "Adds `(1-2)`- `(28-32)`Lightning Damage to Spells",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "Adds {0}- {1}Cold Damage to Spells",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "Adds {0}- {1}Cold Damage to Spells",
+    valueRanges: [
       {
-        "min": 10,
-        "max": 13
+        min: 10,
+        max: 13,
       },
       {
-        "min": 14,
-        "max": 19
-      }
+        min: 14,
+        max: 19,
+      },
     ],
-    "rawAffix": "Adds `(10-13)`- `(14-19)`Cold Damage to Spells"
+    rawAffix: "Adds `(10-13)`- `(14-19)`Cold Damage to Spells",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "Adds {0}- {1}Physical Damage to Spells",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "Adds {0}- {1}Physical Damage to Spells",
+    valueRanges: [
       {
-        "min": 10,
-        "max": 13
+        min: 10,
+        max: 13,
       },
       {
-        "min": 14,
-        "max": 19
-      }
+        min: 14,
+        max: 19,
+      },
     ],
-    "rawAffix": "Adds `(10-13)`- `(14-19)`Physical Damage to Spells"
+    rawAffix: "Adds `(10-13)`- `(14-19)`Physical Damage to Spells",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "Adds {0}- {1}Erosion Damage to Spells",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "Adds {0}- {1}Erosion Damage to Spells",
+    valueRanges: [
       {
-        "min": 11,
-        "max": 13
+        min: 11,
+        max: 13,
       },
       {
-        "min": 15,
-        "max": 18
-      }
+        min: 15,
+        max: 18,
+      },
     ],
-    "rawAffix": "Adds `(11-13)`- `(15-18)`Erosion Damage to Spells"
+    rawAffix: "Adds `(11-13)`- `(15-18)`Erosion Damage to Spells",
   },
   {
-    "equipmentTypeKey": "wand",
-    "equipmentSlot": "One-Handed",
-    "equipmentType": "Wand",
-    "affixType": "Base Affix",
-    "craftingPool": "",
-    "tier": "2",
-    "template": "Adds {0}- {1}Fire Damage to Spells",
-    "valueRanges": [
+    equipmentTypeKey: "wand",
+    equipmentSlot: "One-Handed",
+    equipmentType: "Wand",
+    affixType: "Base Affix",
+    craftingPool: "",
+    tier: "2",
+    template: "Adds {0}- {1}Fire Damage to Spells",
+    valueRanges: [
       {
-        "min": 9,
-        "max": 14
+        min: 9,
+        max: 14,
       },
       {
-        "min": 15,
-        "max": 20
-      }
+        min: 15,
+        max: 20,
+      },
     ],
-    "rawAffix": "Adds `(9-14)`- `(15-20)`Fire Damage to Spells"
-  }
+    rawAffix: "Adds `(9-14)`- `(15-20)`Fire Damage to Spells",
+  },
 ] as const satisfies readonly BaseGearAffix[];
 
 export type WandBaseAffixAffix = (typeof WAND_BASE_AFFIX_AFFIXES)[number];
