7744de079d6f3a965774b7629a0f0ac0fd5015b0
aclghrand <aclghrand@gmail.com>
2025-12-01 00:18:21 -0800

    fix typecheck errors

--- src/data/skill/index.ts
+++ src/data/skill/index.ts
@@ -5,33 +5,3 @@ export * from "./support";
 export * from "./support_magnificent";
 export * from "./support_noble";
 export * from "./activation_medium";
-
-import { ActiveSkills, type ActiveSkill } from "./active";
-import { PassiveSkills, type PassiveSkill } from "./passive";
-import { SupportSkills, type SupportSkill } from "./support";
-import {
-  MagnificentSupportSkills,
-  type MagnificentSupportSkill,
-} from "./support_magnificent";
-import { NobleSupportSkills, type NobleSupportSkill } from "./support_noble";
-import {
-  ActivationMediumSkills,
-  type ActivationMediumSkill,
-} from "./activation_medium";
-
-export const Skills = [
-  ...ActiveSkills,
-  ...PassiveSkills,
-  ...SupportSkills,
-  ...MagnificentSupportSkills,
-  ...NobleSupportSkills,
-  ...ActivationMediumSkills,
-] as const;
-
-export type Skill =
-  | ActiveSkill
-  | PassiveSkill
-  | SupportSkill
-  | MagnificentSupportSkill
-  | NobleSupportSkill
-  | ActivationMediumSkill;
